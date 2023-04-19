abc04391f7886a15a8a4e43dbb0ff30a21a143f6
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2023-04-19 14:05:23 +0300

    tehtävä 3.4

--- index.js
+++ index.js
@@ -51,6 +51,16 @@ app.get('/api/persons/:id', (request, response) => {
     : response.status(404).end()
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    response.status(204).end()
+})
+
+
+
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT)
